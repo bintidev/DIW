9b1757901c7136502286450b5e1b58e6129506e5
B. <bd90962@gmail.com>
2025-10-09 19:37:10 +0200

    Ejercicios Practicos DIW

---
+++ 02-03 etiquetas HTML listas , definiciones y dialogos/galeria/script.js
@@ -0,0 +1,9 @@
+
+document.addEventListener("DOMContentLoaded", function () {
+
+    const dialogo = document.querySelector("miDialogo");
+
+    const imagen = [];
+    const descripcionImg = [];
+
+})
