f5545b6e084b212da28e347d47103621892fc5a4
B. <bd90962@gmail.com>
2025-10-15 20:40:08 +0200

    Completad Ejercicio galeria de HTM listas, definiciones y dialogos

--- Diop_Binta-02-03 HTML listas , definiciones y dialogos/galeria/script.js
+++ Diop_Binta-02-03 HTML listas , definiciones y dialogos/galeria/script.js
@@ -1,9 +1,41 @@
 
+let dialogo = document.getElementById("miDialogo");
+let imagenGrande = document.getElementById("imagenGrande");
+let descripcionImagen = document.getElementById("descripcion");
+let botoncierre = document.getElementById("cerrarDialogo");
 
-function abrir(id) {
+botoncierre.addEventListener("click",() => {
+    dialogo.close();
+});
 
-    cons
+let nombre = [ 
+    "a.jpg",
+    "b.webp",
+    "c.jpg",
+    "d.webp",
+    "e.jpg"
+];
+
+let descripciones = [ 
+    "Montaña",
+    "Chaval",
+    "Colega",
+    "Foto",
+    "Triste"
+];
+
+for (let i = 0; i < nombre.length; i++) {
+
+    let imagenSmall = document.getElementById("img" + (i + 1));
+
+    imagenSmall.addEventListener("click", () => {
+
+        imagenGrande.src="./assets/img/" + nombre[i];
+        descripcionImagen.innerHTML = descripciones[i];
+        dialogo.showModal();
+
+    });
 
 }
-    
+
 
